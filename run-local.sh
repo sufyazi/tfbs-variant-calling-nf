e3d3f5ea1e2eb284d67c41dfd12938d630bf5580
sufyazi <sufyazi@outlook.com>
2024-02-27 16:52:49 +0800

    rebuild docker image of BCFtools

--- run-local.sh
+++ run-local.sh
@@ -4,12 +4,6 @@
 nextflow \
     run \
     main.nf \
-    -profile bedfile_extraction \
-    --run_mode "all" \
-    --bam_dir "test_data/bam_dir" \
-    --genome_fasta "temp_dir/GRCh38_no_alt_GCA_000001405.15.fasta.gz" \
-    --output_dir "test_data/output" # \
-    # 
-    # 
+ 
     # -with-report \
     # -resume
