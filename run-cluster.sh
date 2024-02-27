10deb05beb97f11b84279d3143cca7f546f89791
sufyazi <sufyazi@outlook.com>
2024-02-27 17:27:31 +0800

    update paths

--- run-cluster.sh
+++ run-cluster.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+timestamp=$(date +%Y%m%d-%H.%M)
+
 # Run the workflow on the test data, and write the output to output/
 nextflow \
     run \
-    -resume \
     main.nf \
     --run_mode "subset" \
-    --dataset_id_list "test_data/dataset-manifest-heme-cancers.txt" \
-    --genome_fasta "temp_dir-cluster/refs/GRCh38_no_alt_GCA_000001405.15.fasta.gz" \
-    --output_dir "test_data/output" \
+    --dataset_id_list "input-dir-cluster/manifests/dataset-manifest-test.txt" \
+    --fpscore_matrix "input-dir-cluster/test-matrix" \
+    --genome_fasta "input-dir-cluster/refs/GRCh38_no_alt_GCA_000001405.15.fasta.gz" \
+    --output_dir "output-dir-cluster" \
     -c nextflow_cluster.config \
-    -with-dag
-    # 
+    -with-dag workflow-reports/dag-${timestamp}.html \
+    -with-report workflow-reports/report-${timestamp}.html \
+    -with-timeline workflow-reports/timeline-${timestamp}.html
+    # -resume \
